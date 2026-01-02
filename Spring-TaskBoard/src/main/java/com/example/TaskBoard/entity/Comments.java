58b6f5c94b9239f294d5ff5ac09195bbbbf3f6cf
samueltos <samueltos@yahoo.com>
2026-01-02 11:44:56 -0700

    new commit

--- Spring-TaskBoard/src/main/java/com/example/TaskBoard/entity/Comments.java
+++ Spring-TaskBoard/src/main/java/com/example/TaskBoard/entity/Comments.java
@@ -14,22 +14,92 @@ import java.time.LocalTime;
 public class Comments{
     @Id
     @Column(name = "comment_id", nullable = false)
-    @GeneratedValue(strategy = GenerationType.UUID)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long commentId;
 
     @Column(nullable = false)
     private String comment;
 
-    @Column(nullable = false)
+    @Column(name = "creation_date", nullable = false)
     private LocalDate date;
 
-    @Column(nullable = false)
+    @Column(name = "creation_time", nullable = false)
     private LocalTime time;
 
-    @Column(nullable = false)
-    private User creatorId;
+    @OneToOne
+    @JoinColumn(name = "creator_name", nullable = false)
+    private User user;
 
-    @Column
-    private Issue issueId;
+    @ManyToOne
+    @JoinColumn(name = "issue_id", nullable = false)
+    private Issue issue;
 
+    public Comments(Long commentId, String comment, LocalDate date, LocalTime time, User user, Issue issue) {
+        this.commentId = commentId;
+        this.comment = comment;
+        this.date = date;
+        this.time = time;
+        this.user = user;
+        this.issue = issue;
+    }
+
+    public Long getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(Long commentId) {
+        this.commentId = commentId;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Issue getIssue() {
+        return issue;
+    }
+
+    public void setIssue(Issue issue) {
+        this.issue = issue;
+    }
+
+    @Override
+    public String toString() {
+        return "Comments{" +
+                "commentId=" + commentId +
+                ", comment='" + comment + '\'' +
+                ", date=" + date +
+                ", time=" + time +
+                ", user=" + user +
+                ", issue=" + issue +
+                '}';
+    }
 }
---
+++ Spring-TaskBoard/src/main/java/com/example/TaskBoard/exception/IssueNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.TaskBoard.exception;
+
+public class IssueNotFoundException extends RuntimeException{
+    public IssueNotFoundException(String message){
+        super(message);
+    }
+}
